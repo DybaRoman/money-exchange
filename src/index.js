7cd9836fc563e58d6d31e85d064d024d1fa49308
RomanDyba <romansmaga@mailfence.com>
2019-03-12 17:55:42 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let coins ={H:50, Q:25, D:10, N:5, P:1};
+    if (currency < 1) coins ={}
+    else if (currency > 10000) coins = {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    else for (let c in coins) {
+      let coin = coins[c];
+      coins[c] = Math.floor(currency / coin);
+      currency = currency - coins[c]*coin;
+      if (coins[c] == 0) delete coins[c];
+    }
+    return coins;
 }
